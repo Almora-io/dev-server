41464b28490538ed7b338bc17276048c432725bf
karan k <karankohli13@gmail.com>
2018-06-02 17:21:41 +0530

    changes

--- install.sh
+++ install.sh
@@ -2,41 +2,43 @@
 
 update() {
         echo "Updating";
-                `echo apt-get update`;
+        `echo sudo apt-get update`;
+        `echo export LC_ALL=C;`
 }
 
 install_nginx() {
         echo "Installing nginx";
-                `echo apt-get install nginx`;
+                `echo sudo apt-get install nginx`;
 }
 
 install_mongodb() {
         echo "Installing mongodb";
+                `echo sudo apt-get install  mongodb`
+#                 `echo sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927`;
+#                 `echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list`;
+#                 update;
+#                 `echo sudo apt-get install -y mongodb-org`;
 
-                `echo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927`;
-                `echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list`;
-                update;
-                `echo apt-get install -y mongodb-org`;
-       
 }
 
 install_redis() {
         echo "Installing redis";
 
-                `echo apt-get install build-essential`;
-                `echo apt-get install tcl8.5`;
-                `echo apt-get install redis-server`;
+                `echo sudo apt-get install build-essential`;
+                `echo sudo apt-get install tcl8.5`;
+                `echo sudo apt-get install redis-server`;
 }
 
 install_nodejs() {
         echo "Installing node.js";
 
                 # install node.js
-                `echo apt-get install nodejs`;
+                `echo sudo apt-get install nodejs`;
                 # install nvm
-                `echo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash`;
-       
+                 `echo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash;`
+                 `export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";`
 }
+
 
 
 install_git;
