6ff8643b3791f9630167189807cec7dd687d007d
Karan Kohli <karankohli13@gmail.com>
2018-09-08 04:24:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ install_nodejs() {
                 `echo sudo apt-get install npm`;
                 # install nvm
                  `echo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash;`
-                 `export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";`
+                 `echo export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";`
 }
 
 install_nodemodules(){
